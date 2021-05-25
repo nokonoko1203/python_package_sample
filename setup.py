e259f21bc98519adce5c0ea9ef571d8d65496176
nokonoko1203 <healtheworld.1203@gmail.com>
2021-05-25 22:33:09 +0900

    modify setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 from setuptools import setup
-setup()
+from setuptools import find_packages
+
+
+setup(name='package',
+      version=' 0.0.1',
+      description='Theano-based Deep Learning library',
+      author='nokonoko1203',
+      author_email='sample package',
+      url='https://github.com/nokonoko1203/python_package_template',
+      license='MIT',
+      packages=find_packages())
